9bafea55aaf8c2ba3112c30b8d56fe68057c66b5
nikagw <32739029+nikagw@users.noreply.github.com>
2019-12-12 13:11:05 +0300

    Lesson3

---
+++ src/ru/geekbrains/javaone/lesson_a/online/Lesson3.java
@@ -0,0 +1,135 @@
+package ru.geekbrains.javaone.lesson_a.online;
+
+import javax.swing.*;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lesson3 {
+
+    private static final char DOT_HUMAN = 'X';
+    private static final char DOT_AI = 'O';
+    private static final char DOT_EMPTY = '.';
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final Random RANDOM = new Random();
+    private static char[][] field;
+    private static int fieldSizeX;
+    private static int fieldSizeY;
+
+    // 2 задание (успел только его)
+    private static boolean checkWin(char c) {
+        //проверяем диагонали
+        boolean diagonalLeft = true, diagonalRight = true;
+        for (int i = 0; i < field.length; i++) {
+            diagonalLeft = diagonalLeft & field[i][i] == c;
+            diagonalRight = diagonalRight & field[field.length - i - 1][i] == c;
+        }
+        //проверяем вертикали и горизонтали
+        boolean vertical = true, horizontal = true;
+        for (int i = 0; i < field.length; i++) {
+            vertical = true;
+            horizontal = true;
+            for(int j = 0; j < field.length; j++){
+                vertical = vertical & field[j][i] == c;
+                horizontal = horizontal & field[i][j] == c;
+            }
+            if (vertical || horizontal) break;
+        }
+        return diagonalLeft || diagonalRight || vertical || horizontal;
+    }
+
+    private static void initField() {
+        fieldSizeY = 3;
+        fieldSizeX = 3;
+        field = new char[fieldSizeY][fieldSizeX];
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                field[y][x] = DOT_EMPTY;
+            }
+        }
+    }
+
+    private static void printField() {
+        System.out.print("+");
+        for (int i = 0; i < fieldSizeX * 2 + 1; i++)
+            System.out.print((i % 2 == 0) ? "-" : i / 2 + 1);
+        System.out.println();
+
+        for (int i = 0; i < fieldSizeX; i++) {
+            System.out.print(i + 1 + "|");
+            for (int j = 0; j < fieldSizeX; j++)
+                System.out.print(field[i][j] + "|");
+            System.out.println();
+        }
+
+        for (int i = 0; i < fieldSizeX * 2 + 1; i++)
+            System.out.print("-");
+        System.out.println();
+    }
+
+    private static void humanTurn() {
+        int x;
+        int y;
+        do {
+            System.out.println("Введите координаты X и Y от 1 до 3 через пробел");
+            x = SCANNER.nextInt() - 1;
+            y = SCANNER.nextInt() - 1;
+        } while (!isValidCell(x, y) || !isEmptyCell(x, y));
+        field[y][x] = DOT_HUMAN;
+    }
+
+    private static boolean isValidCell(int x, int y) {
+        return x >= 0 && x < fieldSizeX && y >= 0 && y < fieldSizeY;
+    }
+
+    private static boolean isEmptyCell(int x, int y) {
+        return field[y][x] == DOT_EMPTY;
+    }
+
+    private static void aiTurn() {
+        int x;
+        int y;
+        do {
+            x = RANDOM.nextInt(fieldSizeX);
+            y = RANDOM.nextInt(fieldSizeY);
+        } while (!isEmptyCell(x, y));
+        field[y][x] = DOT_AI;
+    }
+
+
+    private static boolean isDraw() {
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (field[y][x] == DOT_EMPTY)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        initField();
+        printField();
+        while (true) {
+            humanTurn();
+            printField();
+            if (checkWin(DOT_HUMAN)) {
+                System.out.println("Human win!");
+                break;
+            }
+            if (isDraw()) {
+                System.out.println("Draw!");
+                break;
+            }
+            aiTurn();
+            printField();
+            if (checkWin(DOT_AI)) {
+                System.out.println("Computer win!");
+                break;
+            }
+            if (isDraw()) {
+                System.out.println("Draw!");
+                break;
+            }
+        }
+    }
+}
