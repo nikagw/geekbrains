031b2ffaac7a0f3c04188302daa4107eea2221b8
nikagw <32739029+nikagw@users.noreply.github.com>
2019-12-18 13:44:58 +0300

    Lesson 5

--- src/ru/geekbrains/javaone/lesson_a/online/Main.java
+++ src/ru/geekbrains/javaone/lesson_a/online/Main.java
@@ -1,27 +1,43 @@
 package ru.geekbrains.javaone.lesson_a.online;
 
-public class Main {
+import java.util.Random;
+
+public class Main{
     public static void main(String[] args) {
-        Employee employee = new Employee("John", "Java-developer", 10000, 10000, 46);
+        Dog d1 = new Dog("Baton",
+                getRandomIntInRange(400,600),
+                getRandomFloatInRange(0.1f),
+                getRandomIntInRange(9,11));
+        Dog d2 = new Dog("Sharik",
+                getRandomIntInRange(400,600),
+                getRandomFloatInRange(0.5f),
+                getRandomIntInRange(19,21));
+        Horse h = new Horse("Spirit",
+                getRandomIntInRange(1400,1600),
+                getRandomFloatInRange(2.5f),
+                getRandomIntInRange(4,6));
+        Bird b = new Bird("Vorobushek",
+                getRandomIntInRange(4,6),
+                getRandomFloatInRange(0));
+        Cat c = new Cat("Cat in Boots",
+                getRandomIntInRange(150,250),
+                getRandomFloatInRange(1f));
 
-        System.out.println(employee.getName());
-        System.out.println(employee.getPosition());
-        System.out.println(employee.getId());
+        Animal[] zoo = {d1, d2, h, b, c};
 
-        Employee[] employeeArr = new Employee[5];
-        employeeArr[0] = new Employee("Neo", "PHP-developer", 10001,8000,43);
-        employeeArr[1] = new Employee("David", "SEO optimization", 10002,6000,30);
-        employeeArr[2] = new Employee("Sarah", "SMM manager", 10003,5000,25);
-        employeeArr[3] = new Employee("Peter", "QA-tester", 10004,8000,35);
-        employeeArr[4] = new Employee("Adam", "CEO", 10005,10000,50);
-
-        for (Employee i : employeeArr) {
-            if (i.getAge() > 40) {
-                System.out.print("Name:" + i.getName() + " Position:" + i.getPosition() +
-                        " Phone:" + i.getPhoneNumber() + " Salary:" + i.getSalary() +
-                        " Age:" + i.getAge() + " ID:" + i.getId());
-                System.out.println();
-            }
+        for (Animal i : zoo) {
+            i.run(500);
+            i.jump(1);
+            i.swim(6);
+            System.out.println("----------");
         }
+    }
+
+    private static int getRandomIntInRange(int min, int max) {
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+    private static float getRandomFloatInRange(float min) {
+        return  (float) (Math.random() + min);
     }
 }
